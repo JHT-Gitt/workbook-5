a15b4d0d66ac829898da9243eaf94a6a966091f2
Jiward Torres <jiwardtorres@yahoo.com>
2025-05-13 14:56:04 -0700

    new

--- AssetManager/src/main/java/org/example/Main.java
+++ AssetManager/src/main/java/org/example/Main.java
@@ -5,7 +5,18 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Main {
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
     public static void main(String[] args) {
+        boolean isShow1 = false;
+        boolean isShow2 = false;
 
         ArrayList<Asset> a = new ArrayList<>();
 
@@ -35,26 +46,51 @@ public class Main {
 
 
         for( int i = 0 ; i < a.size() ; i++) {
+
             if (a.get(i) instanceof House){
+
+                if(!isShow1){
+                    System.out.printf(YELLOW + "%-15s %-15s %-15s %-30s %-15s %-15s %-15s %-15s\n",
+                            "Description", "Date acquired","Original Cost","Address","Condition","SquareFoot","Lot size","Value" + RESET);
+                    isShow1 = true;
+                }
+
                 House h = (House) a.get(i);
-                System.out.println( "\nDescription: " + h.getDesc());
-                System.out.println("Date acquired: " + h.getDateAcquired());
-                System.out.println("Original Cost: " + h.getOriginalCost());
-                System.out.println("Address: " + h.getAddress());
-                System.out.println("Condition: " + h.getCondition());
-                System.out.println("SquareFoot: " + h.getSquareFoot());
-                System.out.println("Lot size: " + h.getLotSize());
-                System.out.println("Value: " + h.getValue());
+//                System.out.println( "\nDescription: " + h.getDesc());
+//                System.out.println("Date acquired: " + h.getDateAcquired());
+//                System.out.println("Original Cost: " + h.getOriginalCost());
+//                System.out.println("Address: " +  h.getAddress() );
+//                System.out.println("Condition: " + h.getCondition());
+//                System.out.println("SquareFoot: " + h.getSquareFoot());
+//                System.out.println("Lot size: " + h.getLotSize());
+//                System.out.println("Value: " + h.getValue());
+
+
+                System.out.printf("%-15s %-15s %-15.2f %-30s %-15d %-15d %-15d %-15.2f\n",
+                        h.getDesc(), h.getDateAcquired() , h.getOriginalCost(),  h.getAddress() , h.getCondition(),
+                        h.getSquareFoot(),  h.getLotSize(), h.getValue());
+
+
+
             }
             if(a.get(i) instanceof Vehicle){
+                if(!isShow2){
+                    System.out.printf(CYAN + "\n%-15s %-15s %-15s %-15s %-10s %-10s %-15s\n",
+                            "Description","Date Acquired","Original Cost","Make/Model","Year","Odometer","Value" + RESET);
+                    isShow2 = true;
+                }
                 Vehicle v = (Vehicle) a.get(i);
-                System.out.println( "\nDescription: " + v.getDesc());
-                System.out.println("Date acquired: " + v.getDateAcquired());
-                System.out.println("Original Cost: " + v.getOriginalCost());
-                System.out.println("Make/Model: " + v.getMakeModel());
-                System.out.println("Year: " + v.getYear());
-                System.out.println("Odometer: " + v.getOdometer());
-                System.out.println("Value: " + v.getValue());
+//                System.out.println( "\nDescription: " + v.getDesc());
+//                System.out.println("Date acquired: " + v.getDateAcquired());
+//                System.out.println("Original Cost: " + v.getOriginalCost());
+//                System.out.println("Make/Model: " + v.getMakeModel());
+//                System.out.println("Year: " + v.getYear());
+//                System.out.println("Odometer: " + v.getOdometer());
+//                System.out.println("Value: " + v.getValue());
+
+                System.out.printf("%-15s %-15s %-15.2f %-15s %-10d %-10d %-15.2f\n",
+                        v.getDesc(),v.getDateAcquired(), v.getOriginalCost(),v.getMakeModel(),  v.getYear(),v.getOdometer(),v.getValue());
+
             }
 
         }
